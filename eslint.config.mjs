4448bbf03e8419a0088300cde8bdb23999271540
Noé Rivera <tsnoe.dali@gmail.com>
2024-12-21 16:05:12 -0600

    disable eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-var": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
